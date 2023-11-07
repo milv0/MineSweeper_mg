ba9286bddd4eb76961a83a924391d73cd2a643cc
quantbrainz <quantbrainz@gmail.com>
2023-11-07 14:28:11 +0900

    tablelayout_9x9

--- app/src/main/java/com/example/minesweeper_mg/MainActivity.java
+++ app/src/main/java/com/example/minesweeper_mg/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.minesweeper_mg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // 테이블 레이아웃
+        // 테이블 레이아웃 9x9
         table = (TableLayout) findViewById(R.id.tableLayout);
 
+        Button[][] buttons = new Button[9][9];
+        for (int i = 0; i < 9; i++) {
+            TableRow tableRow = new TableRow(this);
+            tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
+            table.addView(tableRow);
+
+            for (int j = 0; j < 9; j++) {
+                buttons[i][j] = new Button(this);
+                TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
+                buttons[i][j].setLayoutParams(layoutParams);
+                tableRow.addView(buttons[i][j]);
+            }
+        }
     }
-
-
 }
