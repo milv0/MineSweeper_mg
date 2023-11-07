3976e684330063ac6721babf0f62634bbb69a95d
milv <uqwe00@gmail.com>
2023-11-07 12:12:53 +0900

    .

--- app/src/main/java/com/example/minesweeper_mg/MainActivity.java
+++ app/src/main/java/com/example/minesweeper_mg/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.minesweeper_mg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TableLayout table;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // 테이블 레이아웃
+        table = (TableLayout) findViewById(R.id.tableLayout);
+
     }
+
+
 }
