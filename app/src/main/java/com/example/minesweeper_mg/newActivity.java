71095b483e3ccbb0dae791e4a71a08fed1b44d77
quantbrainz <quantbrainz@gmail.com>
2023-11-21 09:33:55 +0900

    11/21-class

---
+++ app/src/main/java/com/example/minesweeper_mg/newActivity.java
@@ -0,0 +1,14 @@
+package com.example.minesweeper_mg;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+}
