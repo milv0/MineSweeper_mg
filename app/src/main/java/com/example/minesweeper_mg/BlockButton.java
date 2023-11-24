1c58c8810c4e23462eded98268061946112bd52f
quantbrainz <quantbrainz@gmail.com>
2023-11-24 13:06:26 +0900

    11/24 13:05

--- app/src/main/java/com/example/minesweeper_mg/BlockButton.java
+++ app/src/main/java/com/example/minesweeper_mg/BlockButton.java
@@ -1,36 +1,108 @@
 package com.example.minesweeper_mg;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.widget.TableLayout;
 
+import androidx.annotation.ColorInt;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class BlockButton extends AppCompatButton {
     private int x, y;
     private boolean mine;
-    private boolean flag;
     private int neighborMines;
+    private boolean flag;
+    private boolean check;
 
     // Static fields
-    private static int flags;
-    private static int blocks;
+
+    private static int flags_num;
+    private static int blocks_num;
+    public static int getFlags_num() {
+        return flags_num;     // block 클릭 시 화면 상단에 Flag 개수 띄우기
+    }
+
+    public static int getBlocks_num() {
+        return blocks_num;
+    }
 
     public BlockButton(Context context, int x, int y) {
         super(context);
 
         setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT, 1));
 
-        blocks++;
+        blocks_num++;
 
         this.x = x;
         this.y = y;
         mine = false;
         flag = false;
+        check = false;
         neighborMines = 0;
     }
 
-    public void toggleFlag(){
+    public void checkBlock(){
+        if (!check) {
+            check = true;
+//            flags_num++;
+            setText("V");
+            if(mine){
+                setTextColor(Color.RED);
+                setText("!M!");
+            }
+        } else {
+            check = false;
+//            flags_num--;
+            setText("");
+            if(mine){
+                setTextColor(Color.RED);
+                setText("!M!");
+            }
+        }
+    }
+    public void breakBlock(){
+        setEnabled(false);
+        setText("Bk");
+        check = false;
 
     }
+    public void toggleFlag() {
+        if (!flag) {
+            flag = true;
+            flags_num++;
+            setText("F");
+            setTextColor(Color.RED);
+            setEnabled(false);
+            check = false;
+        } else {
+            flag = false;
+            flags_num--;
+            setText("");
+            setEnabled(false);
+            check = false;
+
+        }
+    }
+
+    public void setMine(boolean b) {
+        mine = b;
+    }
+    public void showAllMines(){
+        setText("!M!");
+    }
+
+    public boolean blockChecked() {
+        return check;
+    }
+    public boolean isFlagged(){
+        return flag;
+    }
+    public  boolean isMine(){
+        return mine;
+    }
+
+
+
 }
--- app/src/main/java/com/example/minesweeper_mg/MainActivity.java
+++ app/src/main/java/com/example/minesweeper_mg/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.minesweeper_mg;
 
+import static com.example.minesweeper_mg.BlockButton.getFlags_num;
+import static com.example.minesweeper_mg.BlockButton.getBlocks_num;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,38 +11,125 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private TableLayout table;
+    private  TextView blocks_num;
+    private BlockButton[][] buttons;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Mine 다 보이기
+
         // 테이블 레이아웃 9x9
         table = (TableLayout) findViewById(R.id.tableLayout);
+        blocks_num = (TextView) findViewById(R.id.blocks_num);
 
-        BlockButton[][] buttons = new BlockButton[9][9];
+        buttons = new BlockButton[9][9];
         for (int i = 0; i < 9; i++) {
             TableRow tableRow = new TableRow(this);
             tableRow.setLayoutParams(new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT));
             table.addView(tableRow);
 
             for (int j = 0; j < 9; j++) {
-                buttons[i][j] = new BlockButton(this,i,j);
+                buttons[i][j] = new BlockButton(this, i, j);
                 TableRow.LayoutParams layoutParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
                 buttons[i][j].setLayoutParams(layoutParams);
                 tableRow.addView(buttons[i][j]);
+
+                buttons[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        ((BlockButton) view).checkBlock();
+//                        blocks_num.setText("Blocks : "+ getBlocks_num());
+                    }
+                });
+            }
+        }
+
+        setMines(buttons);
+
+        blocks_num.setText("Blocks : " + getBlocks_num()); // Blocks 총 개수
+
+        ToggleButton break_button = (ToggleButton)findViewById(R.id.break_button);
+        break_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((ToggleButton) view).isChecked()) {
+                    breakSelectedBlock();
+                    blocks_num.setText("Blocks : "+ getBlocks_num());
+                } else {
+                    flagSelectedBlock();
+//                    ((BlockButton) view).toggleFlag();
+                    TextView textView = (TextView) findViewById(R.id.flags_num); // Block 클릭 시 # 보이기
+                    textView.setText("Flags : " + getFlags_num()); // Block 클릭 시 Flag 개수 BlockButon.java에서 가져오기
+                }
+            }
+        });
+
+    }
+
+    // 지뢰 10개 랜덤 설정
+    private void setMines(BlockButton[][ ] buttons){
+        Random random = new Random();
+        int minesPlaced = 0;
+
+        while (minesPlaced < 10) {
+            int row = random.nextInt(9);
+            int col = random.nextInt(9);
+
+            if (!buttons[row][col].isMine()) {
+                buttons[row][col].setMine(true);
+                minesPlaced++;
             }
         }
     }
 
-    public void onClick(View v){
-        Intent intent = new Intent(getApplicationContext(), newActivity.class);
-        startActivity(intent);
+    // 선택한 블럭 Break
+    private void breakSelectedBlock() {
+        for (int i = 0; i < table.getChildCount(); i++) {
+            View rowView = table.getChildAt(i);
+            if (rowView instanceof TableRow) {
+                TableRow tableRow = (TableRow) rowView;
+                for (int j = 0; j < tableRow.getChildCount(); j++) {
+                    View blockView = tableRow.getChildAt(j);
+                    if (blockView instanceof BlockButton) {
+                        BlockButton selectedBlock = (BlockButton) blockView;
+                        if (selectedBlock.blockChecked()) {
+                            selectedBlock.breakBlock();
+                        }
+                    }
+                }
+            }
+        }
     }
 
+    // 선택한 블럭 Flag
+    private void flagSelectedBlock() {
+        for (int i = 0; i < table.getChildCount(); i++) {
+            View rowView = table.getChildAt(i);
+            if (rowView instanceof TableRow) {
+                TableRow tableRow = (TableRow) rowView;
+                for (int j = 0; j < tableRow.getChildCount(); j++) {
+                    View blockView = tableRow.getChildAt(j);
+                    if (blockView instanceof BlockButton) {
+                        BlockButton selectedBlock = (BlockButton) blockView;
+                        if (selectedBlock.blockChecked()) {
+                            selectedBlock.toggleFlag();
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 }
